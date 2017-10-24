65f38748f3c2f451eebabf25c81d90b946d301a7
Fabio Akita <fabioakita@gmail.com>
2017-10-24 10:33:27 -0200

    Adding pg_restore alias to facilitate Heroku restores

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -211,3 +211,6 @@ alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+
+alias pgr='pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d '
+
